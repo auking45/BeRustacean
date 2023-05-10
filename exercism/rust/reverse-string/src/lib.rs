6ea32f17552d5f0fe5a089ff0a7decb09cfc7e44
auking45 <auking45@gmail.com>
2023-05-10 21:03:58 +0900

    exercism: solve 02. Reverse String #1
    Signed-off-by: auking45 <auking45@gmail.com>

--- exercism/rust/reverse-string/src/lib.rs
+++ exercism/rust/reverse-string/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn reverse(input: &str) -> String {
-    unimplemented!()
+    let mut output = input.to_string();
+    unsafe {
+        let vec = output.as_mut_vec();
+        vec.reverse();
+    }
+
+    output
 }
