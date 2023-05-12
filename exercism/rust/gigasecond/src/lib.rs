d5104d57f5a27a6b87b329837b948cff56b421ed
auking45 <auking45@gmail.com>
2023-05-12 21:59:00 +0900

    exercism: solve 03. Gigasecond #1
    Signed-off-by: auking45 <auking45@gmail.com>

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -1,6 +1,8 @@
 use time::PrimitiveDateTime as DateTime;
+use time::Duration;
 
+const GIGA_SECOND: i64 = 1_000_000_000;
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    unimplemented!("What time is a gigasecond later than {start}");
+    start + Duration::seconds(GIGA_SECOND)
 }
