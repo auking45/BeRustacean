7cd327f9ce1c2a649d1b1f0e2c1f5b66a194ce77
auking45 <auking45@gmail.com>
2023-05-15 20:40:00 +0900

    exercism: solve 04. Clock #1
    Signed-off-by: auking45 <auking45@gmail.com>

--- exercism/rust/clock/src/lib.rs
+++ exercism/rust/clock/src/lib.rs
@@ -1,11 +1,50 @@
-pub struct Clock;
+use std::fmt;
+
+const MIN_AN_HOUR: i32 = 60;
+const MINUTES_A_DAY: i32 = 24 * MIN_AN_HOUR;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        let mut total_min = hours * MIN_AN_HOUR + minutes;
+
+        if total_min < 0 {
+            total_min = MINUTES_A_DAY + (total_min % MINUTES_A_DAY);
+        }
+
+        let hours = (total_min / MIN_AN_HOUR) % 24;
+        let minutes = total_min % MIN_AN_HOUR;
+
+        Self {
+            hours,
+            minutes
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {minutes} minutes to existing Clock time");
+        let mut total_min = self.minutes + minutes;
+
+        if total_min < 0 {
+            total_min = MINUTES_A_DAY + (total_min % MINUTES_A_DAY);
+        }
+
+        let hours = (self.hours + total_min / MIN_AN_HOUR) % 24;
+        let minutes = total_min % MIN_AN_HOUR;
+
+        Self {
+            hours,
+            minutes
+        }
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
