60a98923a993395fdf713c4dc5f6f856d1a7b862
auking45 <auking45@gmail.com>
2023-05-20 15:18:31 +0900

    exercism: solve 05. Anagram #1
    Signed-off-by: auking45 <auking45@gmail.com>

--- exercism/rust/anagram/src/lib.rs
+++ exercism/rust/anagram/src/lib.rs
@@ -1,7 +1,23 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{word}' word find anagrams among the following words: {possible_anagrams:?}"
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let lowercase_word = word.to_lowercase();
+    let mut sorted_word = lowercase_word.chars().collect::<Vec<char>>();
+    sorted_word.sort_unstable();
+
+    let mut output: HashSet<&'a str> = HashSet::new();
+
+    for &s in possible_anagrams.iter() {
+        let lowercase_anagram = s.to_lowercase();
+        let mut sorted_anagram = lowercase_anagram.chars().collect::<Vec<char>>();
+        sorted_anagram.sort_unstable();
+
+        if sorted_word == sorted_anagram {
+            if lowercase_word != lowercase_anagram {
+                output.insert(s);
+            }
+        }
+    }
+
+    output
 }
