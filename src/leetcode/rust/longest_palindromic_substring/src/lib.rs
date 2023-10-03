7fa2214e5f62d7d71148563a89ac4e0ff9ace818
auking45 <auking45@gmail.com>
2023-10-03 18:24:42 +0900

    leetcode: solve 5. Longest Palindromic Substring
    * Time Complexity: O(N^3)
    
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/leetcode/rust/longest_palindromic_substring/src/lib.rs
+++ src/leetcode/rust/longest_palindromic_substring/src/lib.rs
@@ -2,6 +2,27 @@ pub struct Solution {}
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        unimplemented!();
+        let slice_s = s.as_bytes();
+        let mut max_len = 0;
+        let mut start_idx = 0;
+        for i in 0..s.len() {
+            for j in i..s.len() {
+                let mut diff = false;
+
+                for k in 0..((j - i + 1) / 2) {
+                    if slice_s[i + k] != slice_s[j - k] {
+                        diff = true;
+                        break;
+                    }
+                }
+
+                if diff == false && (j - i + 1) > max_len {
+                    start_idx = i;
+                    max_len = j - i + 1;
+                }
+            }
+        }
+
+        String::from_utf8(slice_s[start_idx..start_idx + max_len].to_vec()).unwrap()
     }
 }
