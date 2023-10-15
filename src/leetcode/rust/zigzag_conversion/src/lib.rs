6415d6f48f0dfe7f601df7f1f911dee0125c3080
auking45 <auking45@gmail.com>
2023-10-15 13:13:34 +0900

    leetcode: solve 6. Zigzag Conversion
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/leetcode/rust/zigzag_conversion/src/lib.rs
+++ src/leetcode/rust/zigzag_conversion/src/lib.rs
@@ -2,6 +2,42 @@ pub struct Solution {}
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        unimplemented!();
+        let rows = num_rows as usize;
+        if rows == 1 || rows >= s.len() {
+            return s;
+        }
+
+        let mut result = String::new();
+
+        let added_index = rows * 2 - 1 - 1;
+        let input = s.chars().collect::<Vec<char>>();
+
+        for i in 0..rows {
+            // First position
+            result.push(input[i]);
+
+            // Center and next position 
+            let mut count = 1;
+            loop {
+                if i != 0 && i != (rows - 1) {
+                    let center_index = i + (added_index * count) - (i * 2);
+                    if center_index >= input.len() {
+                        break;
+                    }
+                    result.push(input[center_index]);
+                }
+    
+                let next_index = i + (added_index * count);
+                if next_index >= input.len() {
+                    break;
+                }
+                
+                result.push(input[next_index]);
+
+                count = count + 1;
+            }
+        }
+
+        result
     }
 }
---
+++ src/leetcode/rust/zigzag_conversion/src/main.rs
@@ -0,0 +1,6 @@
+use zigzag_conversion::*;
+
+fn main() {
+    let result = Solution::convert("ABCDE".to_string(), 4);
+    println!("{result}");
+}
