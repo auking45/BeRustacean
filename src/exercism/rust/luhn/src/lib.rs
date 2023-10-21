f39bc6896bf739a0fa2c55c55815d0f8e19001b8
auking45 <auking45@gmail.com>
2023-10-21 11:22:57 +0900

    exercism: solve 07. Luhn #1
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/exercism/rust/luhn/src/lib.rs
+++ src/exercism/rust/luhn/src/lib.rs
@@ -1,4 +1,30 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    // Check if the length of string is less than 2
+    if code.trim().len() < 2 {
+        return false;
+    }
+
+    let mut sum = 0;
+    let mut whitespaces = 0;
+    for (i, c) in code.chars().rev().enumerate() {
+        if c.is_whitespace() {
+            whitespaces += 1;
+        }
+        else if !c.is_numeric() {
+            return false;
+        } else { // numeric
+            let mut num = c as u8 - 48;
+            if (i != 0) && ((i + whitespaces) % 2 == 1) {
+                num = num * 2;
+                if num > 9 {
+                    num -= 9;
+                } 
+            }
+
+            sum += num;
+        }
+    }
+
+    sum % 10 == 0
 }
---
+++ src/exercism/rust/luhn/src/main.rs
@@ -0,0 +1,5 @@
+use luhn::*;
+
+fn main() {
+    is_valid("59");
+}
