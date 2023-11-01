767fd4be6b35f8c32ff7fa1f4bf589eb37d5528f
auking45 <auking45@gmail.com>
2023-11-01 20:52:10 +0900

    exercism: solve 09. Parallel Letter Frequency
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/exercism/rust/parallel-letter-frequency/src/lib.rs
+++ src/exercism/rust/parallel-letter-frequency/src/lib.rs
@@ -1,11 +1,48 @@
+use std::cmp::min;
 use std::collections::HashMap;
+use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
+    let mut answers = HashMap::<char, usize>::new();
+
+    let input = input.concat().to_lowercase();
+    if input.is_empty() {
+        return answers;
+    }
+
+    let workers = min(input.len(), worker_count);
+    let mut work_len = (input.len() / workers).max(1);
+    if input.len() % workers != 0 {
+        work_len += 1;
+    }
+
+    let mut churn = input.chars();
+    let mut handles = Vec::with_capacity(workers);
+    for _ in 0..workers {
+        let chunk = churn.by_ref().take(work_len).collect::<String>();
+        let thread = thread::spawn(move || {
+            let mut answer = HashMap::<char, usize>::new();
+            chunk.chars().for_each(|c| {
+                if c.is_alphabetic() {
+                    answer.entry(c).and_modify(|v| *v += 1).or_insert(1);
+                }
+            });
+
+            answer
+        });
+
+        handles.push(thread);
+    }
+
+    for handle in handles {
+        let answer = handle.join().unwrap();
+        for (key, val) in answer.iter() {
+            answers
+                .entry(*key)
+                .and_modify(|v| *v += val)
+                .or_insert(*val);
         }
-    );
+    }
+
+    answers
 }
