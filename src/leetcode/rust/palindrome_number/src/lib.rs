c58b73df634d4867b6bd97dd0d03ef028d9992be
auking45 <auking45@gmail.com>
2023-11-05 10:13:57 +0900

    leetcode: solve 9. Palindrome Number #1
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/leetcode/rust/palindrome_number/src/lib.rs
+++ src/leetcode/rust/palindrome_number/src/lib.rs
@@ -2,6 +2,46 @@ pub struct Solution {}
 
 impl Solution {
     pub fn is_palindrome(x: i32) -> bool {
-        unimplemented!();
+        if x < 0 {
+            return false;
+        }
+
+        let mut len = 0;
+        let mut back = vec![];
+        let mut x2 = x;
+
+        loop {
+            let val = x2 % 10;
+            back.push(val);
+
+            if val == x2 {
+                break;
+            }
+
+            x2 /= 10;
+            len += 1;
+        }
+
+        let mut forward = vec![];
+        let mut x3 = x;
+
+        loop {
+            let val = x3 / 10i32.pow(len);
+            x3 -= val * 10i32.pow(len);
+
+            forward.push(val);
+
+            if len == 0 {
+                break;
+            }
+
+            len -= 1;
+        }
+
+        if back == forward {
+            true
+        } else {
+            false
+        }
     }
 }
---
+++ src/leetcode/rust/palindrome_number/src/main.rs
@@ -0,0 +1,6 @@
+use palindrome_number::*;
+
+fn main() {
+    Solution::is_palindrome(121);
+    Solution::is_palindrome(45);
+}
