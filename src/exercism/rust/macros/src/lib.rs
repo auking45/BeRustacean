955e203ea8dcd41a29498725f289d3d69bad931b
auking45 <auking45@gmail.com>
2023-11-05 09:41:32 +0900

    exercism: solve 10. Macros
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/exercism/rust/macros/src/lib.rs
+++ src/exercism/rust/macros/src/lib.rs
@@ -1,6 +1,15 @@
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! hashmap {
-    () => {
-        todo!()
-    };
+    ($($k: expr => $v: expr,)+) => { hashmap!($($k => $v),+) };
+    ($($k: expr => $v: expr),*) => {
+        {
+            let mut _map = ::std::collections::HashMap::new();
+
+            $(
+                _map.insert($k, $v);
+            )*
+
+            _map
+        }
+    }
 }
