c2efba85fe0853774bf1664f590c06f56d9fe7ba
auking45 <auking45@gmail.com>
2023-11-14 21:14:18 +0900

    leetcode: solve 10. Regular Expression Matching
    From community solution
    
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/leetcode/rust/regular_expression_matching/src/lib.rs
+++ src/leetcode/rust/regular_expression_matching/src/lib.rs
@@ -2,6 +2,20 @@ pub struct Solution {}
 
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
-        unimplemented!();
+        Solution::is_match_slice(s.as_bytes(), p.as_bytes())
+    }
+
+    fn is_match_slice(s: &[u8], p: &[u8]) -> bool {
+        match (p, s) {
+            ([x, b'*', subp @ ..], [y, subs @ ..]) if *x == b'.' || x == y => {
+                Solution::is_match_slice(subs, p) || Solution::is_match_slice(s, subp)
+            }
+            ([_, b'*', subp @ ..], _) => Solution::is_match_slice(s, subp),
+            ([x, subp @ ..], [y, subs @ ..]) if *x == b'.' || x == y => {
+                Solution::is_match_slice(subs, subp)
+            }
+            ([], s) => s.is_empty(),
+            _ => false,
+        }
     }
 }
--- src/leetcode/rust/regular_expression_matching/tests/test.rs
+++ src/leetcode/rust/regular_expression_matching/tests/test.rs
@@ -14,3 +14,57 @@ fn test_2() {
 fn test_3() {
     assert_eq!(true, Solution::is_match("ab".to_string(), ".*".to_string()));
 }
+
+#[test]
+fn test_4() {
+    assert_eq!(
+        true,
+        Solution::is_match("aaa".to_string(), "a*a".to_string())
+    );
+}
+
+// A &str to String adapter
+fn string_matches_pattern(string: &str, pattern: &str) -> bool {
+    Solution::is_match(string.to_string(), pattern.to_string())
+}
+
+#[test]
+fn test_a_asterisk_a() {
+    assert!(string_matches_pattern("aaa", "a*a"));
+}
+
+#[test]
+fn test_many_zero_repeatables() {
+    assert!(string_matches_pattern("aaa", "ab*a*c*a"));
+}
+
+#[test]
+fn test_multiple_repeated_wildcards() {
+    assert!(string_matches_pattern("bbbba", ".*a*a"));
+}
+
+#[test]
+fn test_only_asterisks_and_dots() {
+    assert!(string_matches_pattern("ab", ".*.."));
+}
+
+#[test]
+fn test_many_asterisks_and_dots_and_other_stuff() {
+    assert!(string_matches_pattern("ab", ".*..c*"));
+}
+
+#[test]
+fn extra_test_case_1() {
+    assert!(string_matches_pattern(
+        "aabcbcbcaccbcaabc",
+        ".*a*aa*.*b*.c*.*a*"
+    ));
+}
+
+#[test]
+fn extra_test_case_2() {
+    assert!(string_matches_pattern(
+        "ccacbcbcccabbab",
+        ".c*a*aa*b*.*b*.*"
+    ));
+}
