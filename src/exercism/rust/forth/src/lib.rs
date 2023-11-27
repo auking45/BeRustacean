26ec9baae14773129c5a87964f501d7346a1a07b
auking45 <auking45@gmail.com>
2023-11-27 22:33:23 +0900

    exercism: solve 11. Forth
    Signed-off-by: auking45 <auking45@gmail.com>

--- src/exercism/rust/forth/src/lib.rs
+++ src/exercism/rust/forth/src/lib.rs
@@ -1,7 +1,14 @@
+use std::collections::HashMap;
+
 pub type Value = i32;
 pub type Result = std::result::Result<(), Error>;
 
-pub struct Forth;
+const INST: [&str; 8] = ["+", "-", "*", "/", "swap", "over", "dup", "drop"];
+
+pub struct Forth {
+    stack: Vec<Value>,
+    custom_inst: HashMap<String, Vec<String>>,
+}
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
@@ -13,14 +20,70 @@ pub enum Error {
 
 impl Forth {
     pub fn new() -> Forth {
-        todo!()
+        Self {
+            stack: Vec::new(),
+            custom_inst: HashMap::new(),
+        }
     }
 
     pub fn stack(&self) -> &[Value] {
-        todo!()
+        self.stack.as_slice()
     }
 
     pub fn eval(&mut self, input: &str) -> Result {
-        todo!("result of evaluating '{input}'")
+        let input = input.to_ascii_lowercase();
+        let mut words = input.split_whitespace();
+
+        while let Some(word) = words.next() {
+            match word {
+                w if w == String::from(":") => {
+                    let name = words.next().unwrap();
+                    let mut def = vec![];
+                    while let Some(w) = words.next() {
+                        if w == ";" {
+                            break;
+                        }
+                        def.push(w.to_string());
+                    }
+                    self.custom_inst.insert(name.to_string(), def.clone());
+                },
+                w if self.custom_inst.contains_key(w) => {
+                    
+                },
+                w if INST.contains(&w) => self.exec_inst(w)?,
+                w if w.parse::<Value>().is_ok() => self.stack.push(w.parse::<Value>().unwrap()),
+                _ => return Err(Error::UnknownWord),
+            }
+        }
+
+        Ok(())
     }
+
+    pub fn exec_inst(&mut self, inst: &str) -> Result {
+        let len = self.stack.len();
+        if len < INST.contains(&inst) as usize + 1 {
+            return Err(Error::StackUnderflow);
+        }
+
+        let elem = self.stack.pop().unwrap();
+        match inst {
+            "+" => self.stack[len - 2] += elem,
+            "-" => self.stack[len - 2] -= elem,
+            "*" => self.stack[len - 2] *= elem,
+            "/" => match elem { 0 => return Err(Error::DivisionByZero), _ => self.stack[len - 2] /= elem },
+            "dup" => self.stack.append(&mut vec![elem, elem]),
+            "swap" => self.stack.insert(len - 2, elem),
+            "over" => self.stack.append(&mut vec![elem, self.stack[len - 2]]),
+            _ => (),
+        }
+
+        Ok(())
+    }
+
+    // pub fn create_custom_inst(&mut self, words: &mut impl Iterator) -> Result {
+    //     while let Some(word) = words.next() {
+    //         println!("{word:?}");
+    //     }
+    //     Ok(())
+    // }
 }
